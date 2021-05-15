427ecfefcd8dfdd32ba4aa9ab8356b49a67787f0
pumpkiny9120 <pumpkiny9120@gmail.com>
2021-05-14 23:01:26 -0700

    Saving/getting posts from Redis.

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const redis = require("redis");
+const redisearch  = require('redis-redisearch');
 const PORT = 5000;
 
-// Connects to local Redis.
+// Connects to local Redis and enable Redisearch commands.
 const redisClient = redis.createClient();
+redisearch(redis);
+// Creates Redisearch index if it does not exist.
+redisClient.ft_create("posts_idx", "ON", "HASH", "PREFIX", "1", "post", "SCHEMA", "content", "TEXT",
+    function (err, response) {
+        if (err && err.message !== "Index already exists") throw err;
+    });
+
 // Sets up application.
 const app = express();
 app.set("view engine", "ejs");
@@ -21,12 +29,25 @@ app.get("/", (req, res) => {
 
 // Posts page for viewing matched posts and selecting a user.
 app.get('/posts', (req, res) =>  {
-    const posts = [{
-        username: 'user1',
-        content: 'It is a nice day out there'
-    }];
-    console.log(posts)
-    res.render('posts', {posts: posts});
+    const username = req.query.username;
+    const myPost = req.query.post;
+    redisClient.hset(`post_${username}`, "username", username, "content", myPost);
+    // TODO find match in Redis
+
+    const posts = [];
+    redisClient.keys("post_*", (err, data) => {
+        let keyCount = data.length;
+        data.map(postKey => {
+            redisClient.hgetall(postKey, (err, data) => {
+                if (username !== data.username) {
+                    posts.push(data);
+                }
+                if (--keyCount === 0) {
+                    res.render('posts', {posts: posts});
+                }
+            });
+        });
+    });
 });
 
 // Chat page for... chatting.
@@ -41,8 +62,6 @@ io.on("connection", socket => {
     loadExistingMessages(socket);
 
     socket.on("message", ({ message, from }) => {
-        console.log(message, from);
-
         // Save the message to Redis
         redisClient.rpush("messages", `${from}:${message}`);
         // Reset TTL to 60 seconds
@@ -64,7 +83,7 @@ function loadExistingMessages(socket) {
                 from: redisUsername
             });
         });
-        if (data.length != 0) {
+        if (data.length !== 0) {
             socket.emit("loadFinished");
         }
     });
